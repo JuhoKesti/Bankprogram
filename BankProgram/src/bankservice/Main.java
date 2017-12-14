b1872cfea7127254569b032ee156c889095d21f4
JuhoKesti <kesti.jp@gmail.com>
2017-12-14 20:27:01 +0200

    third commit
    Deleted some info

--- BankProgram/src/bankservice/Main.java
+++ BankProgram/src/bankservice/Main.java
@@ -3,7 +3,7 @@ package bankservice;
 /*
  * ATM/Bank program
  * OO Programming Java - 2017 Autumn
- * Studentname(nr): Juhopekka Kesti(1601537)
+ * Studentname: Juhopekka Kesti
  * Laurea Leppävaara 
  * Tietojenkäsittely koulutusohjelma/Business information technology
  */
